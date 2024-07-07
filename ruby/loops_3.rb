949b666a34c7762c9de6449229131a9967f72acc
Nilophar Shaik <nilopharshaik@gmail.com>
2024-07-07 18:10:38 -0400

    update loops 3

--- ruby/loops_3.rb
+++ ruby/loops_3.rb
@@ -3,7 +3,10 @@ Write a method that counts down to zero using recursion.
 =end
 
 def countdown(n)
-  return if n < 0 # cannot countdown a negative number
+  if n < 0
+    puts "cannot countdown a negative number #{n}"
+    return
+  end
 
   puts n
   countdown(n-1) if n > 0
