6eabb92ce9c7145698323c45c651c3c3bc1b1464
Nilophar Shaik <nilopharshaik@gmail.com>
2025-02-02 18:31:17 -0500

    rename folder

--- ruby/objects/objects_2.rb
+++ ruby/objects/objects_2.rb
@@ -3,13 +3,15 @@ What is a module? What is its purpose? How do we use them with our classes?
 Create a module for the class you created in exercise 1 and include it properly.
 =end
 
-# Modules are used to methods than are reusable among many similar classes.
+# Modules are used to define methods that are reusable among many similar classes.
+# Modules have to be included, since it cannot be instantiated.
 # It can be used within classes using the include method.
-module Bounce
+
+module Bounceable
 end
 
 class Ball
-  include Bounce
+  include Bounceable
 end
 
 red_ball = Ball.new
